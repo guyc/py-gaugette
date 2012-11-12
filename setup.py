838054b6bd86e4efdaf27e4bcddd1b0229075bec
Guy Carpenter <guy@clearwater.com.au>
2012-11-12 17:23:27 +1000

    standardize source layout

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gaugette',
+      version='1.0',
+      description='Python library for building embedded devices with the Raspberry Pi',
+      author='Guy Carpenter',
+      author_email='guy@clearwater.com.au',
+      url='http://github.com/py-gaugette/',
+      packages=['gaugette', 'gaugette.fonts'],
+)
