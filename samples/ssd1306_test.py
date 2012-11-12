437f7e329ae28b3640a6160d0b4f17556d936e77
Guy Carpenter <guy@clearwater.com.au>
2012-11-12 10:03:32 +1000

    update sample to use updated constant name, remove dead code.

--- samples/ssd1306_test.py
+++ samples/ssd1306_test.py
@@ -5,24 +5,15 @@ import sys
 RESET_PIN = 15
 DC_PIN    = 16
 
-led = gaugette.ssd1306.SSD1306(reset_pin=RESET_PIN, dc_pin=DC_PIN )
+led = gaugette.ssd1306.SSD1306(reset_pin=RESET_PIN, dc_pin=DC_PIN)
 led.begin()
 led.clear_display()
 
 offset = 0 # flips between 0 and 32 for double buffering
 
 while True:
-    # write the current time to the display
-    #    if offset==32:
-    #    print "write date"
-    #    text = time.strftime("%A")
-    #    led.draw_text2(0,offset+0,text,2)
-    #    text = time.strftime("%e %b %y")
-    #    led.draw_text2(0,offset+16,text,2)
-    #else:
-    #    print "write time"
-    #    text = time.strftime("%X")
-    #    led.draw_text2(2,offset+2,text,3)
+
+    # write the current time to the display on every other cycle
     if offset == 0:
         text = time.strftime("%A")
         led.draw_text2(0,0,text,2)
@@ -38,5 +29,5 @@ while True:
     # vertically scroll to switch between buffers
     for i in range(0,32):
         offset = (offset + 1) % 64
-        led.command(led.SETSTARTLINE | offset)
+        led.command(led.SET_START_LINE | offset)
         time.sleep(0.01)
