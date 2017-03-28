c64e3e9cb79de68f180c4f9a3a592d5eed4518a4
Guy Carpenter <guy@clearwater.com.au>
2017-03-29 05:44:00 +1000

    Added detection for RPi3

--- gaugette/platform.py
+++ gaugette/platform.py
@@ -28,6 +28,7 @@ BEAGLEBONE_BLACK = 2
 isBeagleBoneBlack = False
 isRaspberryPi = False
 
+
 def platform_detect():
     """Detect if running on the Raspberry Pi or Beaglebone Black and return the
     platform type.  Will return RASPBERRY_PI, BEAGLEBONE_BLACK, or UNKNOWN."""
@@ -46,7 +47,8 @@ def platform_detect():
         return BEAGLEBONE_BLACK
     elif plat.lower().find('armv7l-with-glibc2.4') > -1:
         return BEAGLEBONE_BLACK
-
+    elif plat.lower().find('armv7l-with-arch') > -1:
+        return BEAGLEBONE_BLACK
     # Couldn't figure out the platform, just return unknown.
     return UNKNOWN
 
@@ -59,10 +61,10 @@ def pi_revision():
         for line in infile:
             # Match a line of the form "Revision : 0002" while ignoring extra
             # info in front of the revsion (like 1000 when the Pi was over-volted).
-            match = re.match(r'Revision\s+:\s+.*(\w{4})$', line, flags=re.IGNORECASE)
+            match = re.match('Revision\s+:\s+.*(\w{4})$', line, flags=re.IGNORECASE)
             if match and match.group(1) in ['0000', '0002', '0003']:
                 # Return revision 1 if revision ends with 0000, 0002 or 0003.
-                return 1
+                return 1ß
             elif match:
                 # Assume revision 2 if revision ends with any other 4 chars.
                 return 2
@@ -71,13 +73,14 @@ def pi_revision():
 
 
 def pi_version():
-    """Detect the version of the Raspberry Pi.  Returns either 1, 2 or
+    """Detect the version of the Raspberry Pi.  Returns either 1, 2, 3 or
     None depending on if it's a Raspberry Pi 1 (model A, B, A+, B+),
-    Raspberry Pi 2 (model B+), or not a Raspberry Pi.
+    Raspberry Pi 2 (model B+), Raspberry Pi 3 or not a Raspberry Pi.
     """
     # Check /proc/cpuinfo for the Hardware field value.
     # 2708 is pi 1
     # 2709 is pi 2
+    # 2835 is pi 3
     # Anything else is not a pi.
     with open('/proc/cpuinfo', 'r') as infile:
         cpuinfo = infile.read()
@@ -93,9 +96,13 @@ def pi_version():
     elif match.group(1) == 'BCM2709':
         # Pi 2
         return 2
+    elif match.group(1) == 'BCM2835':
+        # Pi 3
+        return 3
     else:
         # Something else, not a pi.
         return None
+
 
 platform = platform_detect()
 isBeagleBoneBlack = platform == BEAGLEBONE_BLACK
